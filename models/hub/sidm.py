9cff2cd9b77fac6cd6e28d41e556661d0c3dff1a
janzgraggen <167520096+janzgraggen@users.noreply.github.com>
2025-11-20 14:27:23 +0100

    add fct templates (comented) to complete migration from CerrOroCast

--- models/hub/sidm.py
+++ models/hub/sidm.py
@@ -11,3 +11,37 @@ class dH_to_dT_conv(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+    
+
+# class dH_to_dT_conv_PositionalEncodingPretrained(nn.Module):
+
+
+#     def __init__(self, in_channels=2, out_channels=2):
+#         super().__init__()
+#         self.net = nn.Sequential(
+#             nn.Conv2d(in_channels, 16, kernel_size=3, padding=1),  # local receptive field
+#             nn.ReLU(),
+#             nn.Conv2d(16, out_channels, kernel_size=3, padding=1)  # outputs 2 channels
+#         )
+
+#     def forward(self, x):
+#         return self.net(x)
+    
+
+# class dH_to_dT_conv_PositionalEncodingJointTrained(nn.Module):
+
+
+#     def __init__(self, in_channels=2, out_channels=2):
+#         super().__init__()
+#         self.net = nn.Sequential(
+#             nn.Conv2d(in_channels, 16, kernel_size=3, padding=1),  # local receptive field
+#             nn.ReLU(),
+#             nn.Conv2d(16, out_channels, kernel_size=3, padding=1)  # outputs 2 channels
+#         )
+
+#     def forward(self, x):
+#         return self.net(x)
+    
+    
+
+
