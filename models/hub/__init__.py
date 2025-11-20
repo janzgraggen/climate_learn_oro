d4fa4c8fc3297a08e9c02a39a46087c02fba5742
janzgraggen <167520096+janzgraggen@users.noreply.github.com>
2025-11-20 10:30:54 +0100

    add SIDM models to climate_learn_oro models for importing in metrics

--- models/hub/__init__.py
+++ models/hub/__init__.py
@@ -6,3 +6,4 @@ from .resnet import ResNet
 from .unet import Unet
 from .utils import MODEL_REGISTRY
 from .vit import VisionTransformer, GeoFAR, VisionTransformerCC
+from .sidm import dH_to_dT_conv
